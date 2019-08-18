d84863df1cb3cfa2430991e6a6e17842bce058d6
Cody Smith <codysmith2814@gmail.com>
2019-08-18 13:03:53 -0600

    Added JavaScript functionality to heading

---
+++ main.js
@@ -0,0 +1,8 @@
+var newName = document.querySelector('#newName');
+
+var button = document.querySelector('#button');
+
+
+function showInput () {
+  document.getElementById('newName').innerHTML = document.getElementById('enterName').value;
+}
