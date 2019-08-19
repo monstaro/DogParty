931ebf9f8d65ccf75b820848e415e63d439d9c77
Cody Smith <codysmith2814@gmail.com>
2019-08-18 18:07:25 -0600

    Add text toggle function to buttons

--- main.js
+++ main.js
@@ -6,3 +6,32 @@ var button = document.querySelector('#button');
 function showInput () {
   document.getElementById('newName').innerHTML = document.getElementById('enterName').value;
 }
+
+
+
+function hideText1() {
+  var text = document.getElementById("clear1");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function hideText2() {
+  var text = document.getElementById("clear2");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function hideText3() {
+  var text = document.getElementById("clear3");
+  if (text.style.display === "none") {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
